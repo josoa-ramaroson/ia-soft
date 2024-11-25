c660bca110b3eac3e5c6f7bef938c794a426d4b9
josoa-ramaroson <ramaroson.josoa.r@gmail.com>
2024-11-25 23:16:21 +0300

    update pied.php

--- pied.php
+++ pied.php
@@ -1,4 +1,5 @@
 
 <?php echo 'Copyright COMORES BUSINESS &copy &reg; 2024, AI Makerting. '?>
+
 <?php require 'Journal_audit_v_bd_save.php';?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
